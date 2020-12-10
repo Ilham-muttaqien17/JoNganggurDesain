4d871ef4c0ddb06ee1cb4eed15017e2e9df756c6
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2020-12-10 21:34:42 +0700

    Add project files.

---
+++ JoNganggurDesain/JoNganggurDesain/Views/Dashboard.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JoNganggurDesain.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Dashboard : ContentPage
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        void Rekomendasi(object sender, EventArgs e)
+        {
+            DisplayAlert("Mantap","Ada Bos","OK");
+        }
+    }
+}
---
+++ JoNganggurDesain/JoNganggurDesain/Views/LoginPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using JoNganggurDesain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JoNganggurDesain.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        void Init()
+        {
+            ActivitySpinner.IsVisible = false;
+
+            Entry_Username.Completed += (s, e) => Entry_Password.Focus();
+            Entry_Password.Completed += (s, e) => SignInProcedure(s, e);
+        }
+
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+            if (user.CheckInformation())
+            {
+                DisplayAlert("Login", "Login Sukses", "Oke");
+            } else
+            {
+                DisplayAlert("Login", "Login Gagal", "Oke");
+            }
+        }
+    }
+}
