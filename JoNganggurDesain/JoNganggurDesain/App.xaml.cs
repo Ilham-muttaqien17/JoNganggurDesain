f6a8e4ce56ed2adcce8710ace8d8d42cc9dc4a6c
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2020-12-11 20:19:49 +0700

    FIx SplashScreen

--- JoNganggurDesain/JoNganggurDesain/App.xaml.cs
+++ JoNganggurDesain/JoNganggurDesain/App.xaml.cs
@@ -11,7 +11,7 @@ namespace JoNganggurDesain
         {
             InitializeComponent();
 
-            MainPage = new RegistrasiPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
